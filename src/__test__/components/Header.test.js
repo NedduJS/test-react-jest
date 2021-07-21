1be2813460a8950e8bc109551770a4d94658a1b2
NedduJS <nmamanipantoja@gmail.com>
2021-07-21 09:03:35 -0500

    Product test

---
+++ src/__test__/components/Header.test.js
@@ -0,0 +1,26 @@
+/* eslint-disable comma-dangle */
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+
+import ProviderMock from '../../__mocks__/ProviderMock';
+import Header from '../../components/Header';
+
+describe('<Header />', () => {
+  test('<Header />', () => {
+    const header = shallow(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>
+    );
+    expect(header.length).toEqual(1);
+  });
+
+  test('Render del título', () => {
+    const header = mount(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>
+    );
+    expect(header.find('.Header-title').text()).toEqual('Platzi Store');
+  });
+});
